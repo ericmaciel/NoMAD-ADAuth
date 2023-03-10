7d8c4df22d70e43a5303602864b2d4352b54808f
Eric Maciel <ericmaciel@gmail.com>
2023-03-10 12:41:24 -0300

    Add missing target to the package

--- Package.swift
+++ Package.swift
@@ -4,11 +4,14 @@ import PackageDescription
 let package = Package(
     name: "NoMAD-ADAuth",
     products: [
-        .library(name: "NoMAD-ADAuth", targets: ["NoMAD-ADAuth", "NoMADPRIVATE"])
+        .library(name: "NoMAD-ADAuth", targets: ["NoMAD-ADAuth"]),
     ],
     targets: [
         .target(
             name: "NoMAD-ADAuth",
+            dependencies: [
+                .target(name: "NoMADPRIVATE")
+            ],
             path: "NoMAD-ADAuth",
             exclude: ["ObjC", "FRAMEWORK.md", "Info.plist"]
         ),
